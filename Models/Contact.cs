ee603d6e961f0e4e3895a3fe414b2f06942e9d6b
Enoch Mwangi <enoch.mwangi@outlook.com>
2022-12-07 16:11:34 +0300

    Created Contact class

---
+++ Models/Contact.cs
@@ -0,0 +1,11 @@
+namespace BlazorDemo.APP.Models
+{
+    [Serializable]
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+
+}
